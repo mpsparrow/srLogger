419aa13aaa20d0992cc81470ec19f6f738038e2a
Matthew <webguywp@gmail.com>
2020-01-30 15:33:18 -0500

    fixed typos and added rest of logging

--- srLogger.py
+++ srLogger.py
@@ -1,6 +1,6 @@
 '''
 NAME: srLogger (Server Resource Logger)
-VERSION: 0.2v
+VERSION: 0.3v
 LAST UPDATED: January 30, 2020
 CREATED BY: Matthew
 '''
@@ -23,10 +23,22 @@ def SQLconnect(values):
     else:
         cursor = cnx.cursor()
         try:
-            cursor.execute("CREATE TABLE `usage` (`date` DATE, `time` TIME, `cpu_core_count` INT(255), `cpu_core_count_logical` INT(255), `cpu_percent` DOUBLE(3, 1), `cpu_percent_avg` DOUBLE(3, 1))")
+            cursor.execute("""CREATE TABLE `usage` (
+                `date` DATE, `time` TIME,
+                `cpu_percent_avg` DOUBLE(3, 1), `cpu_min` DOUBLE(3, 1), `cpu_max` DOUBLE(3, 1),
+                `vmemory_percent_avg` DOUBLE(3, 1), `vmemory_min` DOUBLE(3, 1), `vmemory_max` DOUBLE(3, 1),
+                `smemory_percent_avg` DOUBLE(3, 1), `smemory_min` DOUBLE(3, 1), `smemory_max` DOUBLE(3, 1),
+                `storage_percent` DOUBLE(3, 1), `storage_gb_usage` DOUBLE(10, 1)
+                )""")
         except:
             pass
-        query = "INSERT INTO `usage` (date, time, cpu_core_count, cpu_core_count_logical, cpu_percent, cpu_percent_avg) VALUES (%s, %s, %s, %s, %s, %s)"
+        query = """INSERT INTO `usage` (
+            date, time, 
+            cpu_percent_avg, cpu_min, cpu_max,
+            vmemory_percent_avg, vmemory_min, vmemory_max,
+            smemory_percent_avg, smemory_min, smemory_max,
+            storage_percent, storage_gb_usage
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         cursor.execute(query, values)
         cnx.commit()
         cnx.close()
@@ -39,7 +51,6 @@ class Usage:
         self.cpuL = []
         self.vmemoryL = []
         self.smemoryL = []
-        self.storageL = []
 
     def getCoreCount(self):
         return psutil.cpu_count()
@@ -56,14 +67,26 @@ class Usage:
     def getCPUpercentAvg(self):
         return round(sum(self.cpuL)/len(self.cpuL), 1)
 
+    def getCPUmin(self):
+        return min(self.cpuL)
+
+    def getCPUmax(self):
+        return max(self.cpuL)
+
     def getVMemoryAmount(self):
-        return psutil.virtual_memory().total / (1024.0 ** 3)
+        return round(psutil.virtual_memory().total / (1024.0 ** 3), 1)
 
     def getVMemoryPercent(self):
         return psutil.virtual_memory().percent
 
     def getVMemoryPercentAvg(self):
         return round(sum(self.vmemoryL)/len(self.vmemoryL), 1)
+
+    def getVMemoryMin(self):
+        return min(self.vmemoryL)
+
+    def getVMemoryMax(self):
+        return max(self.vmemoryL)
 
     def getSMemoryAmount(self):
         return psutil.swap_memory().total / (1024.0 ** 3)
@@ -74,14 +97,17 @@ class Usage:
     def getSMemoryPercentAvg(self):
         return round(sum(self.smemoryL)/len(self.smemoryL), 1)
 
+    def getSMemoryMin(self):
+        return min(self.smemoryL)
+
+    def getSMemoryMax(self):
+        return max(self.smemoryL)
+
     def getStorageAmount(self):
-        return psutil.disk_usage("/").total / (1024.0 ** 3)
+        return psutil.disk_usage("/").used / (1024.0 ** 3)
 
     def getStoragePercent(self):
         return psutil.disk_usage("/").percent
-
-    def getStoragePercentAvg(self):
-        return round(sum(self.storageL)/len(self.storageL), 1)
 
     def getDate(self):
         return datetime.datetime.now().strftime('%Y-%m-%d')
@@ -94,15 +120,17 @@ class Usage:
             self.cpuL = []
             self.vmemoryL = []
             self.smemoryL = []
-            self.storageL = []
         else:
             self.cpuL.append(self.getCPUpercent())
-            self.vmemoryL.append(self.getVMemoryPercent)
-            self.smemoryL.append(self.getSMemoryPercent)
-            self.storageL.append(self.getStoragePercent)
+            self.vmemoryL.append(self.getVMemoryPercent())
+            self.smemoryL.append(self.getSMemoryPercent())
 
     def logger(self):
-        value = (self.getDate(), self.getTime(),self.getCoreCount(), self.getCoreCountLogical(), self.getCPUpercent(), self.getCPUpercentAvg())
+        value = (self.getDate(), self.getTime())
+        value += (self.getCPUpercentAvg(), self.getCPUmin(), self.getCPUmin())
+        value += (self.getVMemoryPercentAvg(), self.getVMemoryMin(), self.getVMemoryMax())
+        value += (self.getSMemoryPercentAvg(), self.getSMemoryMin(), self.getSMemoryMax())
+        value += (self.getStoragePercent(), self.getStorageAmount())
         SQLconnect(value)
 
     def thread(self):
@@ -116,5 +144,5 @@ class Usage:
                 self.compileInfo()
                 self.x += 1
 
-task1 = Usage(2, 2)
+task1 = Usage(10, 30)
 task1.thread()
